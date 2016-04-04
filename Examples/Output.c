1f00e779e6640494421747220a8d130eb6e9145e
SparkFlame <k.hora2003@gmail.com>
2016-04-04 09:09:35 -0600

    Update Output.c

--- Examples/Output.c
+++ Examples/Output.c
@@ -2,4 +2,14 @@
 #include <avrio.h>
 #include <util/delay.h>
 
+void initDDRB () {
+  DDRB |= 0x01;
+}
 
+int main () {
+  initDDRB(0xFF);
+  while (1){
+    PORTB ^= 0x01;
+    _delay_ms(500);
+  }
+}
