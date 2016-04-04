46f2fbb44674511fabab45c683e7bdf514d2406f
SparkFlame <k.hora2003@gmail.com>
2016-04-04 09:23:12 -0600

    Update and rename Examples/Output.c to Examples/Output/main.c

---
+++ Examples/Output/main.c
@@ -0,0 +1,20 @@
+//Includes
+#include <avrio.h>
+#include <util/delay.h>
+
+//This function will setup DDRB for output on PB0
+//Attach an LED and Resistor to PB0 on 32u4
+void initDDRB () {
+  DDRB |= 0x01;
+}
+
+int main () {
+  //Setup
+  initDDRB(0xFF);    //Calls for setup of PB0
+  
+  //Loop
+  while (1){
+    PORTB ^= 0x01; //Makes PBO blink an led.
+    _delay_ms(500); 
+  }
+}
